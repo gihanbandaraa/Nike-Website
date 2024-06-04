f634ec32e5bbbd4eb78ff482ac784b38a974e6e1
Gihan Bandara <63912749+Gihan1329@users.noreply.github.com>
2024-06-04 19:58:07 +0530

    Services section is completed

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,14 @@
+import ServicesCard from "../components/ServicesCard";
+import { services } from "../constants";
+
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServicesCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
